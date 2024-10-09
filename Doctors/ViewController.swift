7f0b0e1a219668b1f5a82c4cda8d2666b722ee3a
minhnguyen <buivannguyen1312@gmail.com>
2024-10-09 14:45:27 +0700

    Initial Commit

---
+++ Doctors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Doctors
+//
+//  Created by minhnguyen on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DoctorsTests/DoctorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsTests.swift
+//  DoctorsTests
+//
+//  Created by minhnguyen on 9/10/24.
+//
+
+import Testing
+@testable import Doctors
+
+struct DoctorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
